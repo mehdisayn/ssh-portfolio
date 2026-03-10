f322d5113b88e942547a89a5de6ecfcb38c2a66c
Mehdi <m21gits@gmail.com>
2026-03-10 06:40:13 +0600

    working UI

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
+
 import React from "react"
 import { Text } from "ink"
 import figlet from "figlet"
 
-const title = figlet.textSync("MEHDI KAZMI", {
+const title = figlet.textSync("KAZMI", {
  font: "ANSI Shadow"
 })
 
 export default function Header(){
  return <Text color="cyan">{title}</Text>
 }
+
